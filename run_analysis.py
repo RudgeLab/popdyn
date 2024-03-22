446229bd1489bac4a1821569f148a3f4293fd4f4
gyanezfeliu <gyanezfeliu@gmail.com>
2024-03-22 15:18:24 +0000

    function compute_er working for many positions. need to fix plot_er

--- run_analysis.py
+++ run_analysis.py
@@ -18,6 +18,7 @@ from utils import *
 #########################
 # Functions
 #########################
+# this function might be needed in future when the metadata file gets bigger
 def get_params_for_date(microscope, date, metadata):
     """Retrieves all parameters for a given microscope and date.
     
@@ -43,7 +44,8 @@ def get_params_for_date(microscope, date, metadata):
 metadata = {
     'Tweez scope': {
         '2023_11_15': {
-            '0': {'cx': 480, 'cy': 524, 'radius': 100}, #originally taken the other way around, (x, y) instead of (row, col)
+            #'0': {'cx': 524, 'cy': 480, 'radius': 100}, #originally taken the other way around, (x, y) instead of (row, col)
+            '0': {'cx': 480, 'cy': 524, 'radius': 100},
             '1': {'cx': 505, 'cy': 520, 'radius': 100},
             '3': {'cx': 510, 'cy': 510, 'radius': 100},
             # More positions
@@ -71,8 +73,9 @@ metadata = {
 # analysis params
 ##################
 
-#folder = '/home/guillermo/Microscopy/Ti scope'
+#folder = '/home/guillermo/Microscopy'
 folder = '/mnt/ff9e5a34-3696-46e4-8fa8-0171539135be'
+#scope_name = 'Ti scope'
 scope_name = 'Tweez scope'
 path_scope = os.path.join(folder, scope_name)
 exp_date = '2023_11_15'
@@ -82,12 +85,22 @@ folder_results = 'results'
 folder_fluo = 'fluo'
 folder_graphs = 'graphs'
 
+### params for repressilator single reporter
 yfp_chn = 0
 cfp_chn = 1
 ph_chn = 2
 fluo_chns = 2
 
-# create folders
+### params for repressilator triple reporter and pAAA
+"""
+rfp_chn = 0
+yfp_chn = 1
+cfp_chn = 2
+ph_chn = 3
+fluo_chns = 3
+"""
+
+# create folders that will store analysis results
 if not os.path.exists(os.path.join(path, folder_masks)):
     os.makedirs(os.path.join(path, folder_masks))
 if not os.path.exists(os.path.join(path, folder_results)):
@@ -100,8 +113,10 @@ if not os.path.exists(os.path.join(path, folder_graphs)):
 # call get_params_for_date and then make a loop
 colonies = get_params_for_date(scope_name, exp_date, metadata)
 
-for pos in colonies.keys():
-#for pos in range(1,2):
+# loop to perform the functions contour_mask, average_growth, compute_er to each
+# position (colony) selected from an experiment
+#for pos in colonies.keys():
+for pos in range(1):
     # TO DO: fname needs to be more modular
     fname = f'{exp_date}_10x_1.0x_pLPT20&41_single_TiTweez_Pos{pos}.ome.tif'
     fname_mask = 'mask_' + fname
@@ -115,15 +130,31 @@ for pos in colonies.keys():
     im_all = imread(path_im)
     im_ph = im_all[:,:,:,ph_chn].astype(float)
     im_fluo = im_all[:,:,:,:fluo_chns].astype(float)
+    
     ###############
     # contour mask
-    ###############
-
-
-    cx = metadata[scope_name][exp_date][str(pos)]['cx']
-    cy = metadata[scope_name][exp_date][str(pos)]['cy']
-    # TO DO: 'radius' is the guest to start the segmentation
+    # it's in the inverse order because x -> columns and y -> rows
+    cy = metadata[scope_name][exp_date][str(pos)]['cx']
+    cx = metadata[scope_name][exp_date][str(pos)]['cy']
+    # TO DO: 'radius' is the guest to start the segmentation, change this name
     radius = metadata[scope_name][exp_date][str(pos)]['radius']
 
     #contour_mask(im_ph, start_frame, step, pos, cx, cy, radius, path, folder_masks, path_masks)
-    average_growth(im_fluo, path_masks, step, pos, path, folder_results, folder_graphs)
+    
+    ###############
+    # average_growth
+    # average_growth(path_masks, step, pos, path, folder_results, folder_graphs)
+
+    ###############
+    # compute_er
+    # compute_er(im_all, pos, path, folder_results, ph_chn)
+
+    ###############
+    # plot expression rate
+    folder_pos = os.path.join(path, folder_results, f"pos{pos}")
+    er = np.load(os.path.join(folder_pos, 'er.npy'))
+    edt = np.load(os.path.join(folder_pos, 'edt.npy'))
+    sfluo = np.load(os.path.join(folder_pos, 'sfluo.npy'))
+    dsfluo = np.load(os.path.join(folder_pos, 'dsfluo.npy'))
+    nt, nx, ny, nc = sfluo.shape
+    plot_er(im_ph, pos, path, folder_fluo, er, edt, sfluo, dsfluo)
--- utils.py
+++ utils.py
@@ -14,7 +14,7 @@ from skimage.draw import polygon
 from skimage.transform import warp_polar
 from skimage.transform import warp, EuclideanTransform
 
-from scipy.ndimage import distance_transform_edt
+from scipy.ndimage import distance_transform_edt, gaussian_filter
 from scipy.optimize import fmin, least_squares
 from scipy.signal import savgol_filter
 from scipy.io import savemat
@@ -47,14 +47,17 @@ def bg_corr(im, rmin, rmax, cmin, cmax):
     bg = im[0,rmin:rmax, cmin:cmax,:].mean(axis=(1,2))
     nt,nx,ny,nc = im.shape
     for t in range(nt):
+        print(f'Smoothing frame {t+1}/{nt}')
         for c in range(nc):
-            im[t,:,:,c] = im[t,:,:,c] - bg[c]
+            im[t,:,:,c] = gaussian_filter(im[t,:,:,c] - bg[c], 8)
     im[im<0] = 0
+
     return im
 
 def contour_mask(im_ph, start_frame, step, pos, cx, cy, radius, path, folder_masks, path_masks):
     nt,nx,ny = im_ph.shape
     mask_out = np.zeros((nt,) + (nx,ny))
+
     temp_folder = os.path.join(path, folder_masks,f"temp_pos{pos}")
     if not os.path.exists(temp_folder):
         os.makedirs(temp_folder)
@@ -105,7 +108,7 @@ def contour_mask(im_ph, start_frame, step, pos, cx, cy, radius, path, folder_mas
     make_video(os.path.join(path, folder_masks,f"temp_pos{pos}"), 
            os.path.join(path, folder_masks,f"contour_pos{pos}.avi"))
 
-def average_growth(im_fluo, path_masks, step, pos, path, folder_results, folder_graphs):
+def average_growth(path_masks, step, pos, path, folder_results, folder_graphs):
     
     folder_pos = os.path.join(path, folder_results,f"pos{pos}")
     if not os.path.exists(folder_pos):
@@ -114,9 +117,6 @@ def average_growth(im_fluo, path_masks, step, pos, path, folder_results, folder_
     folder_pos_graph = os.path.join(path, folder_graphs,f"pos{pos}")
     if not os.path.exists(folder_pos_graph):
         os.makedirs(folder_pos_graph)
-    
-    # BG correction using first frame, too much scattering in later frames
-    im_fluo = bg_corr(im_fluo, 0, 100, 0, 100)
 
     mask_all = imread(path_masks)
     mask_all = mask_all>0
@@ -131,11 +131,11 @@ def average_growth(im_fluo, path_masks, step, pos, path, folder_results, folder_
     for t in range(nt):
         edt[t,:,:] = distance_transform_edt(mask_all[t,:,:])
 
-    np.save(os.path.join(path, folder_results, folder_pos, 'radius.npy'), radius)
-    np.save(os.path.join(path, folder_results, folder_pos, 'area.npy'), area) 
-    np.save(os.path.join(path, folder_results, folder_pos, 'vfront.npy'), vfront)
-    np.save(os.path.join(path, folder_results, folder_pos, 'expansion_rate.npy'), exprate)
-    np.save(os.path.join(path, folder_results, folder_pos, 'edt.npy'),  edt)
+    np.save(os.path.join(folder_pos, 'radius.npy'), radius)
+    np.save(os.path.join(folder_pos, 'area.npy'), area) 
+    np.save(os.path.join(folder_pos, 'vfront.npy'), vfront)
+    np.save(os.path.join(folder_pos, 'expansion_rate.npy'), exprate)
+    np.save(os.path.join(folder_pos, 'edt.npy'),  edt)
 
     plt.subplot(4,1,1)
     plt.plot(area)
@@ -145,5 +145,194 @@ def average_growth(im_fluo, path_masks, step, pos, path, folder_results, folder_
     plt.plot(vfront)
     plt.subplot(4,1,4)
     plt.plot(exprate)
-    plt.savefig(os.path.join(path, folder_results, folder_pos_graph, 'a_r_vf_exp.png'))
+    plt.savefig(os.path.join(folder_pos_graph, 'a_r_vf_exp.png'))
     plt.close()
+
+def register(im, edt):
+    nt,nx,ny,nc = im.shape
+    y,x = np.meshgrid(np.arange(ny), np.arange(nx))
+
+    # Register time series
+    edt0 = edt[-1,:,:]
+    cx0 = x[edt0>0].mean()
+    cy0 = y[edt0>0].mean()
+    for t in range(nt):
+        print(f'Registering frame {t+1}/{nt}')
+        tedt = edt[t,:,:]
+        cx = y[tedt>0].mean()
+        cy = x[tedt>0].mean()
+        shift = [cx0 - cx, cy0 - cy]
+        tform = EuclideanTransform(translation=shift)
+        
+        # register euclidean distance
+        regtedt = warp(tedt, tform.inverse, preserve_range=True)
+        edt[t,:,:] = regtedt
+        
+        # register all channels
+        for c in range(nc):
+            ctim = im[t,:,:,c]
+            regctim = warp(ctim, tform.inverse, preserve_range=True)
+            im[t,:,:,c] = regctim
+            #plt.subplot(1, nc, c+1)
+            #plt.imshow(regctim)
+        #plt.savefig('regim_%04d.png'%t)
+        #plt.close()
+    return im, edt
+
+def compute_er(im, pos, path, folder_results, ph_chn):
+    folder_pos = os.path.join(path, folder_results, f"pos{pos}")
+    nt, nx, ny, nc = im.shape
+    tmin = 0
+    tmax = nt
+
+    area = np.load(os.path.join(folder_pos, 'area.npy'))
+    radius = np.sqrt(area/np.pi)
+    # TO DO: not used nor saved
+    dsarea = savgol_filter(area, 21, 3, deriv=1)
+    sradius = savgol_filter(radius, 21, 3)
+    dsradius = savgol_filter(radius, 21, 3, deriv=1)
+
+    np.save(os.path.join(folder_pos, 'sradius.npy'), sradius)
+    np.save(os.path.join(folder_pos, 'dsradius.npy'), dsradius)
+    #savemat(os.path.join(path_res, 'sradius.mat'), {'sradius':sradius})
+    #savemat(os.path.join(path_res, 'dsradius.mat'), {'dsradius':dsradius})
+
+    edt = np.load(os.path.join(folder_pos, 'edt.npy'))
+    edt = edt[tmin:tmax,:,:]
+    im = im[tmin:tmax,:,:,:]
+    nt,nx,ny,nc = im.shape
+
+    # register image
+    im, edt = register(im, edt)
+    # save im and edt registered
+    np.save(os.path.join(folder_pos, 'im_reg.npy'), im)
+    np.save(os.path.join(folder_pos, 'edt_reg.npy'), edt)
+    
+    # test
+    #imsave(os.path.join(path, folder_results, folder_pos, 'im_reg.ome.tif'), im)
+
+    # select a ROI of the image to analyze
+    y,x = np.meshgrid(np.arange(ny), np.arange(nx))
+    edt0 = edt[-1,:,:]
+    xmin = x[edt0>0].min() - 32
+    xmax = x[edt0>0].max() + 32
+    ymin = y[edt0>0].min() - 32
+    ymax = y[edt0>0].max() + 32
+
+    edt = edt[:,xmin:xmax,ymin:ymax]
+    nt,nx,ny = edt.shape
+    x,y = np.meshgrid(np.arange(ny), np.arange(nx))
+
+    # change phase channel index
+    ph = im[:,xmin:xmax,ymin:ymax,ph_chn]
+
+    # BG correction and gaussian smoothing
+    fluo = im[:,:,:,:ph_chn]
+    fluo = bg_corr(fluo, 0, 100, 0, 100)
+
+    fluo = fluo[:,xmin:xmax,ymin:ymax,:]
+    nt,nx,ny,nc = fluo.shape
+
+    sfluo = savgol_filter(fluo, 31, 3, axis=0)
+    dsfluo = savgol_filter(fluo, 31, 3, deriv=1, axis=0)
+    np.save(os.path.join(folder_pos, 'sfluo.npy'), sfluo)
+    np.save(os.path.join(folder_pos, 'dsfluo.npy'), dsfluo)
+    #savemat(os.path.join(path, folder_results, folder_pos, 'sfluo.mat'), {'sfluo':sfluo})
+    #savemat(os.path.join(path, folder_results, folder_pos, 'dsfluo.mat'), {'dsfluo':dsfluo})
+
+    #############################################################################
+    ### Expression rate computation
+    gamma = np.log(2) / (12 * 60 / 10)
+    er = dsfluo + gamma * sfluo
+    vmin = [0]*3 #np.nanmin(er, axis=(0,1,2))
+    vmax = np.nanmax(er, axis=(0,1,2))
+    corr = np.zeros((nt,))
+    for t in range(nt):
+        print(f'Computing correlations frame {t+1}/{nt}')
+        tedt = edt[t,:,:]
+        x = er[t,:,:,0]
+        y = er[t,:,:,1]
+        x = x[tedt>0]
+        y = y[tedt>0]
+        x = (x - vmin[0]) / (vmax[0] - vmin[0])
+        y = (y - vmin[1]) / (vmax[1] - vmin[1])
+        corr[t] = np.sum(x * y) / np.sqrt(np.sum(x*x) + np.sum(y*y))
+        #np.corrcoef(x[tedt>0].ravel(), y[tedt>0].ravel())[0,1]
+    np.save(os.path.join(folder_pos, 'er.npy'), er)
+    np.save(os.path.join(folder_pos, 'corr.npy'), corr)
+
+def plot_er(im_ph, pos, path, folder_fluo, er, edt, sfluo, dsfluo):
+    folder_pos = os.path.join(path, folder_fluo, f"pos{pos}")
+    if not os.path.exists(folder_pos):
+        os.makedirs(folder_pos)    
+
+    vmin = [0]*3
+    vmax = np.nanmax(er, axis=(0,1,2))
+
+    nt, nx, ny = edt.shape
+    y,x = np.meshgrid(np.arange(ny), np.arange(nx))
+    edt0 = edt[-1,:,:]
+    xmin = x[edt0>0].min() - 32
+    xmax = x[edt0>0].max() + 32
+    ymin = y[edt0>0].min() - 32
+    ymax = y[edt0>0].max() + 32
+
+    # TO DO: make edt.shape and ph.shape match with sfluo.shape 
+    edt = edt[:,xmin:(xmax+1),ymin:(ymax+1)]
+    ph = im_ph[:,xmin:(xmax+1),ymin:(ymax+1)]
+    #############################################
+
+    nt, nx, ny = edt.shape
+    _, _, _, nc = sfluo.shape
+
+    print(f"edt: {edt.shape}")
+    print(f"sfluo: {sfluo.shape}")
+    print(f"ph: {ph.shape}")
+
+    for t in range(nt):
+        print(f"Plotting dsfluo {t+1} / {nt}")
+        plt.figure(figsize=(9,3))
+        for c in range(nc):
+            plt.subplot(1,nc,c+1)
+            tcdsfluo = dsfluo[t,:,:,c]
+            tcdsfluo[edt[t,:,:]==0] = np.nan
+            plt.imshow(tcdsfluo)
+            plt.colorbar()
+        plt.tight_layout()
+        plt.savefig(os.path.join(folder_pos, 'dsfluo_%04d.png'%t))
+        plt.close()
+
+    for t in range(nt):
+        print(f"Plotting sfluo {t+1} / {nt}")
+        plt.figure(figsize=(9,3))
+        for c in range(nc):
+            plt.subplot(1,nc,c+1)
+            tcsfluo = sfluo[t,:,:,c]
+            tcsfluo[edt[t,:,:]==0] = np.nan
+            plt.imshow(tcsfluo)
+            plt.colorbar()
+        plt.tight_layout()
+        plt.savefig(os.path.join(folder_pos, 'sfluo_%04d.png'%t))
+        plt.close()
+
+    for t in range(nt):
+        print(f"Plotting er {t+1} / {nt}")
+        plt.figure(figsize=(9,3))
+        plt.subplot(1, nc+2, 1)
+        plt.imshow(ph[t,:,:], cmap='gray')
+        nter = np.zeros((nx,ny,3))
+        for c in range(nc):
+            ntcer = np.zeros((nx,ny,3))
+            plt.subplot(1,nc+2,c+2)
+            tcer = er[t,:,:,c]
+            tcer[edt[t,:,:]==0] = np.nan
+            #nter[:,:,c] = (tcer - np.nanmin(tcer)) / (np.nanmax(tcer)  - np.nanmin(tcer))
+            nter[:,:,c] = (tcer - vmin[c]) / (vmax[c]  - vmin[c])
+            ntcer[:,:,c] = nter[:,:,c]
+            plt.imshow(ntcer)
+            #plt.colorbar()
+        plt.subplot(1, nc+2, nc+2)
+        plt.imshow(nter)
+        plt.tight_layout()
+        plt.savefig(os.path.join(folder_pos, 'er_%04d.png'%t))
+        plt.close()
