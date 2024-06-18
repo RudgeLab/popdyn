c344c86568774d1854596786143a2eb4cfe9cf43
gyanezfeliu <gyanezfeliu@gmail.com>
2024-06-18 22:32:05 +0100

    velocity fitting and covariance

--- popdyn/compute_velocity.py
+++ popdyn/compute_velocity.py
@@ -11,34 +11,38 @@ import os
 
 # Parameters -----------------------------------
 #folder = '/media/c1046372/Expansion/Conor MSc/Analysis_2/DHL708/14-06-23_pLPT20/Position 0'
-folder = '/home/campus.ncl.ac.uk/c1046372/Microscopy'
-
+#folder = '/home/campus.ncl.ac.uk/c1046372/Microscopy'
+folder = '/media/guillermo/Expansion/Thesis GY/3. Analyzed files'
 path = folder+'/'+'velocity_data'
 
 #scope_name = 'Ti scope'
-scope_name = 'Ti scope'
+scope_name = 'Tweez scope'
 path_scope = os.path.join(folder, scope_name)
-exp_date = '2023_11_30'
+exp_date = '2023_12_08'
 path = os.path.join(path_scope, exp_date)
 folder_masks = 'contour_masks'
 folder_results = 'results'
 
-startframe = 30
+startframe = 290
 step = 1
-nframes = 5
+nframes = 20
+#nframes = 50
 nt = nframes-1
 
-windowsize = 64
-windowspacing = 32
+#windowsize = 64
+#windowspacing = 32
+windowsize = 32
+windowspacing = 16
 window_px0 = 0
 window_py0 = 0
 
 maxvel = 19
 
 #------------------------------------------------
-im = imread(folder+'/'+'10x_1.0x_pLPT20_DHL_1_MMStack_Pos0.ome.tif')
-im = im[:,:,:,0]
-mask = imread(folder+'/'+'contour_masks/120_frames/10x_1.0x_pLPT20_DHL_1_MMStack_Pos0.contour.mask.tif')
+#im = imread(folder+'/'+'10x_1.0x_pLPT20_DHL_1_MMStack_Pos0.ome.tif')
+im = imread(os.path.join(path,folder_results,'pos19','crop_im_all.tif'))
+im = im[:,:,:,1]
+mask = imread(os.path.join(path,folder_results,'pos19','crop_edt.tif'))
 init_vel = np.zeros(mask.shape + (2,))
 mask = mask / mask.max() # Make sure 0-1
 im = im[startframe:startframe+(nframes * step):step,:,:]
